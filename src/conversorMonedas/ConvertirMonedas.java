bb5f0834cf6dec72fda4a73221d9bfd0cde8cb51
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-07-25 00:22:25 -0600

    Update ConvertirMonedas.java
    Se agrega comentarios para facilitar el entendimiento del código.

--- src/conversorMonedas/ConvertirMonedas.java
+++ src/conversorMonedas/ConvertirMonedas.java
@@ -4,34 +4,53 @@ import javax.swing.JOptionPane;
 
 public class ConvertirMonedas {
 
-	
+	// Convertir pesos a dólares
 	public void ConvertirPesosADolares(double valor) {
+		// Tasa de conversión de pesos a dólares
 		double monedaDolar = valor / 3739.00;
-		monedaDolar = (double) Math.round(monedaDolar *100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaDolar+ " Dolares");
+		// Redondear el resultado a dos decimales
+		monedaDolar = (double) Math.round(monedaDolar * 100d) / 100;
+        // Mostrar el monto convertido en un cuadro de diálogo
+		JOptionPane.showMessageDialog(null, "Tienes $ " + monedaDolar + " Dolares");
 	}
 	
+	// Convertir pesos a euros
 	public void ConvertirPesosAEuros(double valor) {
+		// Tasa de conversión de pesos a euros
 		double monedaEuro = valor / 4050.48;
-		monedaEuro = (double) Math.round(monedaEuro *100d)/100;
-		JOptionPane.showMessageDialog(null, "Tienes $ " +monedaEuro+ " Euros");
+		// Redondear el resultado a dos decimales
+		monedaEuro = (double) Math.round(monedaEuro * 100d) / 100;
+		// Mostrar el monto convertido en un cuadro de diálogo
+		JOptionPane.showMessageDialog(null, "Tienes $ " + monedaEuro + " Euros");
 	}
 	
+	// Convertir pesos a libras esterlinas
 	public void ConvertirPesosALibras(double valor) {
+		// Tasa de conversión de pesos a libras esterlinas
 		double monedaLibra = valor / 4890.52;
-        monedaLibra = (double) Math.round(monedaLibra *100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaLibra+ " Libras Esterlinas");
+		// Redondear el resultado a dos decimales
+        monedaLibra = (double) Math.round(monedaLibra * 100d) / 100;
+        // Mostrar el monto convertido en un cuadro de diálogo
+        JOptionPane.showMessageDialog(null, "Tienes $ " + monedaLibra + " Libras Esterlinas");
 	}
 	
+	// Convertir pesos a yenes
 	public void ConvertirPesosAYen(double valor) {
+		// Tasa de conversión de pesos a yenes
 		double monedaYen = valor / 29.68;
-        monedaYen = (double) Math.round(monedaYen *100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaYen+ " Yuanes");
+		// Redondear el resultado a dos decimales
+        monedaYen = (double) Math.round(monedaYen * 100d) / 100;
+        // Mostrar el monto convertido en un cuadro de diálogo
+        JOptionPane.showMessageDialog(null, "Tienes $ " + monedaYen + " Yuanes");
 	}
 	
+	// Convertir pesos a wons
 	public void ConvertirPesosAWon(double valor) {
+		// Tasa de conversión de pesos a wons
 		double monedaWon = valor / 3.04;
-        monedaWon = (double) Math.round(monedaWon *100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaWon+ " Wons");
+		// Redondear el resultado a dos decimales
+        monedaWon = (double) Math.round(monedaWon * 100d) / 100;
+        // Mostrar el monto convertido en un cuadro de diálogo
+        JOptionPane.showMessageDialog(null, "Tienes $ " + monedaWon + " Wons");
 	}
 }
